46a1e91872a5aea8dca6d8233a60110fe6a83ade
Sibel Kaya <sibelkaya@MBP-von-Sibel.fritz.box>
2026-01-17 22:32:01 +0100

    fix 3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 
 // Loggt jede Request
 app.use((req, res, next) => {
-  console.log(${new Date().toISOString()} ${req.method} ${req.url});
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
   next();
 });
 
